3cb2726aafb8eb26c3cd0c90df371ceec382b72f
svastikkka <ManshuSharma143Repo@gmail.com>
2022-10-25 23:57:24 +0530

    populating fake data using faker inside sqlite

---
+++ sixth_project/populate_sixth_app.py
@@ -0,0 +1,43 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sixth_project.settings")
+
+import django
+
+django.setup()
+
+# Fake population script
+import random
+from sixth_app.models import AccessRecord, Topic, Webpage
+from faker import Faker
+
+fakegen = Faker()
+topics = ['Search', 'Social', 'News', 'Games', 'Marketplace']
+
+
+def add_topic():
+    t = Topic.objects.get_or_create(topic_name=random.choice(topics))[0]
+    t.save()
+    return t
+
+
+def populate(N=5):
+    for entry in range(N):
+        # get the topic for the entry
+        fake_top = add_topic()
+        # Create fake data
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        # create fake webpage entry
+        webpg = Webpage.objects.get_or_create(topic=fake_top, url=fake_url, name=fake_url)[0]
+
+        # Create fake access records
+        acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fake_date)[0]
+
+
+if __name__ == '__main__':
+    print('populating script')
+    populate(20)
+    print('populating complete')
